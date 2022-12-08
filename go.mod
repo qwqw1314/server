9bceda2f4567861f30b902eda6b16c874ceb2b6a
qwqw1314 <qwqw1314@gmail.com>
2022-12-08 14:50:29 +0900

    v0.0.4

--- go.mod
+++ go.mod
@@ -3,8 +3,23 @@ module echo
 go 1.18
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
+	gorm.io/driver/postgres v1.4.5
+	gorm.io/gorm v1.24.2
+)
+
+require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+
+	"gorm.io/driver/postgres"
+        "gorm.io/gorm"
+        "gorm.io/gorm/logger"
 )
 
+type Ips struct {
+        gorm.Model
+        IP       string
+        Hostname string
+}
+
 func main() {
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.POST("/", fromAgent)
+	e.POST("/agent", fromAgent)
 
 	e.Logger.Fatal(e.Start(":5678"))
 }
@@ -24,5 +36,27 @@ func fromAgent(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusNotFound, "Failed")
 	}
+	host := string(body)
+
+	dsn := "host=10.2.13.132 user=admin password=admin dbname=postgresdb port=5432"
+        db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+                Logger: logger.Default.LogMode(logger.Info),
+        })
+        if err != nil {
+                panic(err)
+        }
+        db.AutoMigrate(&Ips{})
+
+	var ips Ips
+
+	ip := strings.Split(host, "\t")[0]
+	hostname := strings.Split(host, "\t")[1]
+
+	result := db.First(&ips, "Hostname = ?", hostname)
+
+	if result.RowsAffected == 0 {
+		db.Create(&Ips{IP: ip, Hostname: hostname})
+	}
+
 	return c.String(http.StatusOK, string(body))
 }
